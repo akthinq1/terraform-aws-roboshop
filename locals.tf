be03d66d2c92bd2f46dc4b0eccea1c85c512b86a
anil kumar <akthinq@gmailcom>
2025-07-09 16:09:15 +0530

    module for roboshop components

---
+++ locals.tf
@@ -0,0 +1,23 @@
+locals {
+  ami_id             = data.aws_ami.joindevops.id
+  sg_id              = data.aws_ssm_parameter.sg_id.value
+  private_subnet_id  = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+
+
+
+  common_tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    Terraform   = true
+  }
+
+  tg_port           = "${var.component}" == "frontend" ? 80 : 8080
+  health_check_path = "${var.component}" == "forntend" ? "/" : "/health"
+
+  alb_listener_arn = "${var.component}" == "frontend" ? data.aws_ssm_parameter.frontend_alb_listener_arn.value : data.aws_ssm_parameter.backend_alb_listener_arn.value
+
+  rule_header_url= "${var.component}" == "frontend" ? "${var.environment}.${var.zone_name}" : "${var.component}.backend-${var.environment}.${var.zone_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "zone_name" {
+  default = "akdevops.fun"
+}
+
+variable "component" {
+  type = string
+}
+
+variable "rule_priority" {
+  
+}
